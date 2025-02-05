b98b0da851cfe712f8a27bfa720e368a3f1df8ed
Jack Wang <133188103+ks-jackwang@users.noreply.github.com>
2025-02-05 16:13:16 +0800

    新增安裝 ComfyUI 的腳本，包含虛擬環境設置及 systemd 服務配置

---
+++ install-comfyui.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Assign the first parameter to ACTIVEUSER
+ACTIVEUSER=$1
+
+# Set echo on
+set -x
+export NEEDRESTART_MODE=a 
+
+# Set up the ComfyuI
+cd /github
+mkdir ComfyUI
+chown $ACTIVEUSER:$ACTIVEUSER ComfyUI
+su -c "git clone https://github.com/comfyanonymous/ComfyUI.git" $ACTIVEUSER
+
+# Create a virtual environment and install requirements
+su -c "python3 -m venv /github/ComfyUI/venv && \
+source /github/ComfyUI/venv/bin/activate && \
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simpl && \
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126 && \
+pip install -r /github/ComfyUI/requirements.txt" $ACTIVEUSER
+
+# Create the systemd service file default port 8188
+sudo cat <<EOF > /etc/systemd/system/ComfyUI.service
+[Unit]
+Description=ComfyUI
+After=network.target
+
+[Service]
+User=$ACTIVEUSER
+Group=$ACTIVEUSER
+WorkingDirectory=/github/ComfyUI
+ExecStart=/github/ComfyUI/venv/bin/python main.py --port 8188
+Restart=always
+Environment="PATH=/github/ComfyUI/venv/bin:/usr/bin:/usr/local/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+ 
+# Reload systemd daemon and enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable stable-diffusion-webui.service
+
+sudo reboot
